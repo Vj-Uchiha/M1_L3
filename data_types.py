8e3ae1fb9a2008e990d21e36f71ea9dba2f0d48d
Vj-Uchiha <vjam1601@gmail.com>
2026-05-30 16:26:23 +0530

    initial commit

---
+++ data_types.py
@@ -0,0 +1,6 @@
+#finding are of a rectangle using explicit data type conversion
+length=int(input("What is the length? "))
+width=float(input("What is the breadth? "))
+area=length*width
+print("The area is", area)
+print(type(area))
